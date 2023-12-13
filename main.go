152d3ecdab7ce6eea24c0ca4d0c22acee75cb416
satoshi <satoshi@bitcoin.org>
2023-12-13 19:25:28 +0800

    feature: subcommand: parseTransaciton

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"brc20defi_vm/bitcoin_cli_channel"
 	"brc20defi_vm/inscription_parser"
 	"brc20defi_vm/updater"
+	"errors"
 	"flag"
 )
 
@@ -26,14 +27,30 @@ func parseRawTransaction(parseRawTransactionString string) {
 	}
 }
 
+func parseTransaction(txId string) error {
+	rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
+	if rawTx == nil {
+		err := errors.New("GetRawTransaction Failed")
+		return err
+	}
+	parseRawTransaction(*rawTx)
+	return nil
+}
+
 func main() {
 	println("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.")
 
+	var parseTransactionString string
+	flag.StringVar(&parseTransactionString, "parsetransaction", "", "brc20defi_vm -parsetransaction [txid]")
 	var parseRawTransactionString string
-	flag.StringVar(&parseRawTransactionString, "parserawtransaction", "", "Parse Raw Transaction")
+	flag.StringVar(&parseRawTransactionString, "parserawtransaction", "", "brc20defi_vm -parserawtransaction [raw transaction string]")
 	flag.Parse()
-
-	if parseRawTransactionString != "" {
+	if parseTransactionString != "" {
+		err := parseTransaction(parseTransactionString)
+		if err != nil {
+			println("parseTransaction error:", err)
+		}
+	} else if parseRawTransactionString != "" {
 		parseRawTransaction(parseRawTransactionString)
 	} else {
 		updateIndex()
