9a1d5a168f237e9faac505698d951dbb0f2d3283
satoshi <satoshi@bitcoin.org>
2023-12-14 14:59:16 +0800

    feature: vm: virtual machine memory(db table) definitions

--- virtual_machine/virtual_machine_memory.go
+++ virtual_machine/virtual_machine_memory.go
@@ -1,16 +1,79 @@
 package virtual_machine
 
-type DeployedCoins struct {
+import "math/big"
+
+/*
+CoinList
+@dbpath db.coin
+@desc Store all coins' name deployed by OpDeploy.
+*/
+type CoinList struct {
+	names []string
 }
 
+/*
+CoinMeta
+@db_path db.coin_[coin-name]
+@desc Store a coin's metadata deployed.
+@writing_op OpDeploy, OpMint
+*/
+type CoinMeta struct {
+	supply          big.Float
+	minted          big.Float
+	limitPerMint    big.Float
+	limitPerAccount big.Float
+	deployedTx      string
+}
+
+/*
+CoinBalance
+@db_path db.coin_[coin-name]_[address]
+@desc Store a coin's balance at a single account.
+@writing_op OpMint, OpTransfer, OpAddLiquidity, OpRemoveLiquidity OpSwap
+*/
 type CoinBalance struct {
+	value big.Float
 }
 
+/*
+Transactions
+@db_path db.tx_[coin-name]_[instruction-id]
+@desc Store a coin's transactions
+@writing_op OpDeploy, OpMint, OpTransfer, OpAddLiquidity, OpRemoveLiquidity OpSwap
+*/
+type Transactions struct {
+	txId   string
+	op     string
+	params map[string]interface{}
+}
+
+/*
+LiquidityPairList
+@db_path db.lp_[coin-name]
+@desc Store all liquidity pairs associated with a coin
+@writing_op OpAddLiquidity
+*/
+type LiquidityPairList struct {
+	pairs []string
+}
+
+/*
+LiquidityPair
+@db_path db.lp_[left-coin-name|right-coin-name]
+@desc Store liquidity pair metadata, totalValue, price
+@writing_op OpAddLiquidity, OpRemoveLiquidity, OpSwap
+*/
+type LiquidityPair struct {
+	total big.Float
+	price big.Float
+}
+
+/*
+LiquidityPairBalance
+@db_path db.lp_[left-coin-name|right-coin-name]_[address]
+@desc Store liquidity pair balance of a single value
+@writing_op OpAddLiquidity, OpRemoveLiquidity, OpSwap
+*/
 type LiquidityPairBalance struct {
-}
-
-type VoteStatus struct {
-}
-
-type RootMemory struct {
+	value big.Float
 }
