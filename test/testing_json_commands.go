11bcc2d7d5988d23d4915df53b870cf18307d3d3
satoshi <satoshi@bitcoin.org>
2023-12-15 05:07:11 +0800

    feature: unit test: adding test case of json commands

---
+++ test/testing_json_commands.go
@@ -0,0 +1,17 @@
+package test
+
+func TestingMultiCommandsAndTxid() ([]byte, string) {
+	command := `[
+		{"p":"orddefi","op":"deploy","tick":"odfi","max":"21000000","lim":"1000","alim":"1000","icon":""},
+		{"p":"orddefi","op":"mint","tick":"odfi","amt":"1000"},
+		{"p":"orddefi","op":"mint","tick":"odgv","amt":"1000"},
+		{"p":"orddefi","op":"transfer","tick":"odfi","amt":"1000"},
+		{"p":"orddefi","op":"transfer","tick":"odfi","amt":"1000", "to": "bc1qm34lsc65zpw79lxes69zkqmk6ee3ewf0j77s3h"},
+		{"p":"orddefi","op":"addlp","ltick":"odfi","rtick":"odgv","lamt":"1000","ramt":"1000"},
+		{"p":"orddefi","op":"addlp","ltick":"odfi","rtick":"odgv","lamt":"1000","ramt":"1000","as":0},
+		{"p":"orddefi","op":"swap","ltick":"odfi","rtick":"odgv","spend":"odgv","amt":"10","trhd":"0.8%"},
+		{"p":"orddefi","op":"rmlp","ltick":"odfi","rtick":"odgv","amt":"10"}
+	]`
+	txid := ""
+	return []byte(command), txid
+}
