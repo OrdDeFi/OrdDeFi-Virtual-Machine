4dc8738681cd0f44f03d7d8fff1122fb4860d57d
satoshi <satoshi@bitcoin.org>
2023-12-17 17:42:33 +0800

    feature: storage: db_utils and db unit test

--- db_utils/db_utils.go
+++ db_utils/db_utils.go
@@ -1,1 +1,77 @@
 package db_utils
+
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/util"
+	"log"
+)
+
+type OrdDB struct {
+	db *leveldb.DB
+}
+
+func OpenDB(path string) (*OrdDB, error) {
+	levelDB, err := leveldb.OpenFile(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	ordDB := OrdDB{
+		db: levelDB,
+	}
+
+	return &ordDB, nil
+}
+
+func CloseDB(db *OrdDB) {
+	err := db.db.Close()
+	if err != nil {
+		log.Println("Failed to close DB:", err)
+	}
+}
+
+/*
+Keeps string-in-string-out principle in all public data functions.
+Cast from []byte to string automatically make a copy on []byte, to get avoid of stack memory issue and data interrupted.
+*/
+
+func (db OrdDB) Store(key string, value string) error {
+	err := db.db.Put([]byte(key), []byte(value), nil)
+	return err
+}
+
+func (db OrdDB) Read(key string) (*string, error) {
+	byteRes, err := db.db.Get([]byte(key), nil)
+	if err != nil {
+		return nil, err
+	}
+	res := string(byteRes)
+	return &res, nil
+}
+
+func (db OrdDB) ReadAllPrefix(prefixString string) (map[string]string, error) {
+	prefix := []byte(prefixString)
+
+	iter := db.db.NewIterator(util.BytesPrefix(prefix), nil)
+	defer iter.Release()
+
+	var result map[string]string
+	result = make(map[string]string)
+	for iter.Next() {
+		key := iter.Key()
+		value := iter.Value()
+		result[string(key)] = string(value)
+	}
+	if err := iter.Error(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (db OrdDB) StoreKeyValues(keyValues map[string]string) error {
+	batch := new(leveldb.Batch)
+	for key, value := range keyValues {
+		batch.Put([]byte(key), []byte(value))
+	}
+	err := db.db.Write(batch, nil)
+	return err
+}
---
+++ test/db_test.go
@@ -0,0 +1,113 @@
+package test
+
+import (
+	"OrdDefi-Virtual-Machine/db_utils"
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestDB(t *testing.T) {
+	db, err := db_utils.OpenDB("./test_db")
+	if err != nil {
+		t.Errorf("TestDB OpenDB error: %s", err.Error())
+	}
+	defer db_utils.CloseDB(db)
+	fmt.Println("DB opened successfully.")
+
+	key := "coin_list"
+	valueString := `["odfi","odgv"]`
+	err = db.Store(key, valueString)
+	if err != nil {
+		t.Errorf("TestDB error: %s", err.Error())
+	}
+	resultString, err := db.Read(key)
+	if err != nil {
+		t.Errorf("TestDB error: %s", err.Error())
+	}
+	if *resultString != valueString {
+		t.Errorf("TestDB error: result %s, expected %s", *resultString, valueString)
+	}
+}
+
+func TestDBRead(t *testing.T) {
+	db, err := db_utils.OpenDB("./test_db")
+	if err != nil {
+		t.Errorf("TestDBRead OpenDB error: %s", err.Error())
+	}
+	defer db_utils.CloseDB(db)
+	fmt.Println("DB opened successfully.")
+
+	key := "coin_list"
+	valueString := `["odfi","odgv"]`
+
+	resultString, err := db.Read(key)
+	if err != nil {
+		t.Errorf("TestDB error: %s", err.Error())
+	}
+	if *resultString != valueString {
+		t.Errorf("TestDB error: result %s, expected %s", *resultString, valueString)
+	}
+}
+
+func TestDBReadPrefix(t *testing.T) {
+	db, err := db_utils.OpenDB("./test_db")
+	if err != nil {
+		t.Errorf("TestDBReadPrefix OpenDB error: %s", err.Error())
+	}
+	defer db_utils.CloseDB(db)
+	fmt.Println("DB opened successfully.")
+	prefix := "coin:odfi:"
+	for i := 0; i < 20; i++ {
+		key := prefix + strconv.Itoa(i)
+		value := key + ":value"
+		err = db.Store(key, value)
+		if err != nil {
+			t.Errorf("TestDBReadPrefix Store error: %s", err.Error())
+		}
+	}
+	res, err := db.ReadAllPrefix(prefix)
+	if err != nil {
+		t.Errorf("TestDBReadPrefix ReadAllPrefix error: %s", err.Error())
+	}
+	for key, valueString := range res {
+		if valueString != key+":value" {
+			t.Errorf("TestDBReadPrefix ReadAllPrefix value: %s, expected %s", valueString, key+":value")
+		}
+	}
+}
+
+func TestDBBatchStore(t *testing.T) {
+	db, err := db_utils.OpenDB("./test_db")
+	if err != nil {
+		t.Errorf("TestDBBatchStore OpenDB error: %s", err.Error())
+	}
+	defer db_utils.CloseDB(db)
+	fmt.Println("DB opened successfully.")
+
+	var keyValues map[string]string
+	keyValues = make(map[string]string)
+	for i := 0; i < 20; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := key + ":value"
+		keyValues[key] = value
+	}
+	err = db.StoreKeyValues(keyValues)
+	if err != nil {
+		t.Errorf("TestDBBatchStore StoreKeyValues error: %s", err.Error())
+	}
+	for i := 0; i < 20; i++ {
+		key := "key" + strconv.Itoa(i)
+		expectedValue := key + ":value"
+		value, err := db.Read(key)
+		if err != nil {
+			t.Errorf("TestDBBatchStore Read error: %s", err.Error())
+		}
+		if value == nil {
+			t.Errorf("TestDBBatchStore Read error: value empty")
+		}
+		if expectedValue != *value {
+			t.Errorf("TestDBBatchStore value error: %s, expected %s", *value, expectedValue)
+		}
+	}
+}
