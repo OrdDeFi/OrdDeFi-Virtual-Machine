01b24ba1490ef4cce406461b53c4cc80029a492f
satoshi <satoshi@bitcoin.org>
2023-12-21 05:03:41 +0800

    test: test case definition for OpTransfer

---
+++ test/execute_optransfer_test.go
@@ -0,0 +1,43 @@
+package test
+
+import "testing"
+
+func testDirectIllegalTick(t *testing.T) {
+
+}
+
+func testDirectBalanceNotEnough(t *testing.T) {
+
+}
+
+func testDirectNormalTransfer(t *testing.T) {
+
+}
+
+func TestDirectTransfer(t *testing.T) {
+	testDirectIllegalTick(t)
+	testDirectBalanceNotEnough(t)
+	testDirectNormalTransfer(t)
+}
+
+func testUTXOIllegalTick(t *testing.T) {
+
+}
+
+func testUTXOBalanceNotEnough(t *testing.T) {
+
+}
+
+func testUTXONormalTransfer(t *testing.T) {
+
+}
+
+func TestUTXOTransfer(t *testing.T) {
+	testUTXOIllegalTick(t)
+	testUTXOBalanceNotEnough(t)
+	testUTXONormalTransfer(t)
+}
+
+func TestUTXOTransferArriving(t *testing.T) {
+
+}
