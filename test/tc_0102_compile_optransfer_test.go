2db54003d86dbab1e1cf2aa5429b3dddb7953e01
satoshi <satoshi@bitcoin.org>
2023-12-25 05:55:31 +0800

    test: test case for "to" param of OpTransfer. The value of "to" should be a valid address

--- test/tc_0102_compile_optransfer_test.go
+++ test/tc_0102_compile_optransfer_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"OrdDeFi-Virtual-Machine/bitcoin_cli_channel"
 	"OrdDeFi-Virtual-Machine/tx_utils"
+	"OrdDeFi-Virtual-Machine/virtual_machine"
 	"testing"
 )
 
@@ -32,5 +33,45 @@ func TestParseCoinbaseTxInput(t *testing.T) {
 		if address != nil || inputValue != nil {
 			t.Errorf("TestParseCoinbaseTxInput ParseInputAddressAndValue error: for coinbase tx address and inputValue should be nil")
 		}
+	}
+}
+
+func TestValidToParam(t *testing.T) {
+	txId := TestingTxPool()[0]
+	commands := `{"p":"orddefi","op":"mint","tick":"odfi","amt":"1000","to":"bc1qm34lsc65zpw79lxes69zkqmk6ee3ewf0j77s3h"}`
+	rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
+	if rawTx == nil {
+		t.Errorf("TestCommandParse GetRawTransaction error")
+	}
+	tx := bitcoin_cli_channel.DecodeRawTransaction(*rawTx)
+	if tx == nil {
+		t.Errorf("TestCommandParse DecodeRawTransaction error")
+	}
+	instructions, err := virtual_machine.CompileInstructions("text/plain", []byte(commands), tx, txId)
+	if err != nil {
+		t.Errorf("TestCommandParse CompileInstructions error: %s", err.Error())
+	}
+	if len(instructions) != 1 {
+		t.Errorf("TestCommandParse instructions count is not 1")
+	}
+}
+
+func TestInValidToParam(t *testing.T) {
+	txId := TestingTxPool()[0]
+	commands := `{"p":"orddefi","op":"transfer","tick":"odfi","amt":"1000","to":"abcd"}`
+	rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
+	if rawTx == nil {
+		t.Errorf("TestCommandParse GetRawTransaction error")
+	}
+	tx := bitcoin_cli_channel.DecodeRawTransaction(*rawTx)
+	if tx == nil {
+		t.Errorf("TestCommandParse DecodeRawTransaction error")
+	}
+	instructions, err := virtual_machine.CompileInstructions("text/plain", []byte(commands), tx, txId)
+	if err != nil {
+		t.Errorf("TestCommandParse CompileInstructions error: %s", err.Error())
+	}
+	if len(instructions) != 0 {
+		t.Errorf("TestCommandParse instructions count is not 1")
 	}
 }
