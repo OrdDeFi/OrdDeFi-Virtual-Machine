91b472dfdb15eb2230e671fd695d8f9d44d9b5bd
satoshi <satoshi@bitcoin.org>
2023-12-26 21:47:15 +0800

    test: test case for compiling OpSwap

--- test/tc_0105_compile_opswap_test.go
+++ test/tc_0105_compile_opswap_test.go
@@ -8,16 +8,16 @@ import (
 	"testing"
 )
 
-func testingCompileRemoveLP(t *testing.T, commands string) (*string, *string, *safe_number.SafeNum) {
+func testingCompileSwap(t *testing.T, commands string) (*string, *string, *safe_number.SafeNum) {
 	txId := "61de96170018ce878b1adf287b8ac9cf0e4f0ad8c5a69af203cc25bbde72a13e"
 	rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
 	if rawTx == nil {
-		t.Errorf("testingCompileRemoveLP GetRawTransaction error")
+		t.Errorf("testingCompileSwap GetRawTransaction error")
 		return nil, nil, nil
 	}
 	tx := bitcoin_cli_channel.DecodeRawTransaction(*rawTx)
 	if tx == nil {
-		t.Errorf("testingCompileRemoveLP DecodeRawTransaction error")
+		t.Errorf("testingCompileSwap DecodeRawTransaction error")
 		return nil, nil, nil
 	}
 	instructions, err := virtual_machine.CompileInstructions("text/plain", []byte(commands), tx, txId)
@@ -26,23 +26,23 @@ func testingCompileRemoveLP(t *testing.T, commands string) (*string, *string, *s
 		return nil, nil, nil
 	}
 	if len(instructions) != 1 {
-		t.Errorf("testingCompileRemoveLP CompileInstructions error: instructions length should be 1")
+		t.Errorf("testingCompileSwap CompileInstructions error: instructions length should be 1")
 		return nil, nil, nil
 	}
 	for _, instruction := range instructions {
 		switch value := instruction.(type) {
-		case instruction_set.OpRemoveLiquidityProviderInstruction:
+		case instruction_set.OpSwapInstruction:
 			return value.ExtractParams()
 		default:
-			t.Errorf("testingCompileRemoveLP error: instruction type error, expected OpAddLiquidityProviderInstruction")
+			t.Errorf("testingCompileSwap error: instruction type error, expected OpAddLiquidityProviderInstruction")
 		}
 	}
 	return nil, nil, nil
 }
 
-func TestCompileInvalidRemoveLP(t *testing.T) {
-	commands := `[{"p":"orddefi","op":"rmlp","ltick":"ODFI","rtick":"ODFI","amt":"1001"}]`
-	ltick, rtick, amt := testingCompileRemoveLP(t, commands)
+func TestCompileInvalidSwap(t *testing.T) {
+	commands := `[{"p":"orddefi","op":"swap","ltick":"ODFI","rtick":"ODFI","spend":"ODFI","amt":"1001"}]`
+	ltick, rtick, amt := testingCompileSwap(t, commands)
 	if ltick != nil {
 		t.Errorf("error: ltick should be nil")
 	}
@@ -54,9 +54,23 @@ func TestCompileInvalidRemoveLP(t *testing.T) {
 	}
 }
 
-func TestCompileValidRemoveLP(t *testing.T) {
-	commands := `[{"p":"orddefi","op":"rmlp","ltick":"ODFI","rtick":"ODGV","amt":"1001"}]`
-	ltick, rtick, amt := testingCompileRemoveLP(t, commands)
+func TestCompileInvalidSwap2(t *testing.T) {
+	commands := `[{"p":"orddefi","op":"swap","ltick":"ODFI","rtick":"ODGV","spend":"NULL","amt":"1001"}]`
+	ltick, rtick, amt := testingCompileSwap(t, commands)
+	if ltick != nil {
+		t.Errorf("error: ltick should be nil")
+	}
+	if rtick != nil {
+		t.Errorf("error: rtick should be nil")
+	}
+	if amt != nil {
+		t.Errorf("error: lamt should be nil")
+	}
+}
+
+func TestCompileValidSwap(t *testing.T) {
+	commands := `[{"p":"orddefi","op":"swap","ltick":"ODFI","rtick":"ODGV","spend":"ODFI","amt":"1001"}]`
+	ltick, rtick, amt := testingCompileSwap(t, commands)
 	if *ltick != "odfi" {
 		t.Errorf("error: ltick should be odfi")
 	}
@@ -68,9 +82,9 @@ func TestCompileValidRemoveLP(t *testing.T) {
 	}
 }
 
-func TestCompileValidRemoveLP2(t *testing.T) {
-	commands := `[{"p":"orddefi","op":"rmlp","ltick":"ODGV","rtick":"ODFI","amt":"1001"}]`
-	ltick, rtick, amt := testingCompileRemoveLP(t, commands)
+func TestCompileValidSwap2(t *testing.T) {
+	commands := `[{"p":"orddefi","op":"swap","ltick":"ODGV","rtick":"ODFI","spend":"ODFI","amt":"1001"}]`
+	ltick, rtick, amt := testingCompileSwap(t, commands)
 	if *ltick != "odfi" {
 		t.Errorf("error: ltick should be odfi")
 	}
