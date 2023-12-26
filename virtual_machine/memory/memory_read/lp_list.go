1aa4de18886364a6b5b9b152962081aff810c4a3
satoshi <satoshi@bitcoin.org>
2023-12-27 04:13:39 +0800

    feature: db: read LP list

--- virtual_machine/memory/memory_read/lp_list.go
+++ virtual_machine/memory/memory_read/lp_list.go
@@ -1,11 +1,30 @@
 package memory_read
 
-import "OrdDeFi-Virtual-Machine/db_utils"
+import (
+	"OrdDeFi-Virtual-Machine/db_utils"
+	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_const"
+	"encoding/json"
+)
 
 /*
 AllLiquidityProviders
 Read all liquidity pairs
 */
 func AllLiquidityProviders(db *db_utils.OrdDB) ([]string, error) {
-	return nil, nil
+	r, err := db.Read(memory_const.LpListTable)
+	if err != nil && err.Error() != "leveldb: not found" {
+		return nil, err
+	}
+	var liquidityProviders []string
+	if r != nil {
+		var result []string
+		err2 := json.Unmarshal([]byte(*r), &result)
+		if err2 != nil {
+			return nil, err
+		}
+		if result != nil && len(result) != 0 {
+			liquidityProviders = append(liquidityProviders, result...)
+		}
+	}
+	return liquidityProviders, nil
 }
