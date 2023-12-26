5bcf45e98e33b6c5a0643b322bee1760a32c01c0
satoshi <satoshi@bitcoin.org>
2023-12-27 03:45:22 +0800

    feature: db: liquidity pair balance double write path method

--- virtual_machine/memory/memory_write/lp_balance_double_write.go
+++ virtual_machine/memory/memory_write/lp_balance_double_write.go
@@ -1,1 +1,14 @@
 package memory_write
+
+import "OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_const"
+
+func LPBalanceDoubleWriteKV(lCoin string, rCoin string, address string, newBalanceString string) map[string]string {
+	balanceKey1 := memory_const.LPAddressPath(lCoin, rCoin, address)
+	balanceKey2 := memory_const.AddressLPPath(lCoin, rCoin, address)
+	// Generate batch writing map
+	var batchWriting map[string]string
+	batchWriting = make(map[string]string)
+	batchWriting[balanceKey1] = newBalanceString
+	batchWriting[balanceKey2] = newBalanceString
+	return batchWriting
+}
