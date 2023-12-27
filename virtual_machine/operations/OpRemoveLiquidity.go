37742001d5964df397701d054bc47210d0e30c35
satoshi <satoshi@bitcoin.org>
2023-12-28 03:44:35 +0800

    feature: vm: impl of OpRemoveLiquidity

--- virtual_machine/operations/OpRemoveLiquidity.go
+++ virtual_machine/operations/OpRemoveLiquidity.go
@@ -3,12 +3,49 @@ package operations
 import (
 	"OrdDeFi-Virtual-Machine/db_utils"
 	"OrdDeFi-Virtual-Machine/virtual_machine/instruction_set"
+	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_const"
+	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_read"
+	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_write"
 	"errors"
+	"fmt"
 )
+
+func removeLP(instruction instruction_set.OpRemoveLiquidityProviderInstruction, db *db_utils.OrdDB, lpMeta *memory_const.LPMeta) error {
+	address := instruction.TxOutAddr
+	lTick, rTick, consumingLPAmount := instruction.ExtractParams()
+	x := lpMeta.LAmt
+	y := lpMeta.RAmt
+	total := lpMeta.Total
+	lpRatio := consumingLPAmount.DivideBy(total)
+	if lpRatio == nil {
+		return fmt.Errorf("calulate lpRatio error: %s / %s", consumingLPAmount.String(), total.String())
+	}
+	addingLAmt := x.Multiply(lpRatio)
+	if addingLAmt == nil {
+		return fmt.Errorf("calulate addingLAmt error: %s * %s", x.String(), lpRatio.String())
+	}
+	addingRAmt := y.Multiply(lpRatio)
+	if addingRAmt == nil {
+		return fmt.Errorf("calulate addingRAmt error: %s * %s", y.String(), lpRatio.String())
+	}
+	err := memory_write.WriteRemoveLPInfo(db, *lTick, *rTick, addingLAmt, addingRAmt, consumingLPAmount, lpMeta, address)
+	return err
+}
 
 func ExecuteOpRemoveLiquidityProvider(instruction instruction_set.OpRemoveLiquidityProviderInstruction, db *db_utils.OrdDB) error {
 	if instruction.TxInAddr != instruction.TxOutAddr {
 		return errors.New("no privileges on cross-address remove liquidity provider")
 	}
-	return nil
+	lTick, rTick, consumingLPAmount := instruction.ExtractParams()
+	if lTick == nil || rTick == nil || consumingLPAmount == nil {
+		return errors.New("ExecuteOpRemoveLiquidityProvider error: params extracting error")
+	}
+	lpMeta, err := memory_read.LiquidityProviderMetadata(db, *lTick, *rTick)
+	if err != nil {
+		return err
+	}
+	if lpMeta == nil {
+		return errors.New("ExecuteOpRemoveLiquidityProvider error: LP not exist")
+	}
+	return removeLP(instruction, db, lpMeta)
 }
