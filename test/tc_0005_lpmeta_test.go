70e00f4e593a2d858e5e71ad9cf1d5c616dbb0f4
satoshi <satoshi@bitcoin.org>
2023-12-28 23:11:29 +0800

    test: test case for read LPMeta

---
+++ test/tc_0005_lpmeta_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"OrdDeFi-Virtual-Machine/db_utils"
+	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_read"
+	"fmt"
+	"testing"
+)
+
+func TestLPMeta(t *testing.T) {
+	// open db
+	db, err := db_utils.OpenDB("./test_db")
+	if err != nil {
+		t.Errorf("TestExecuteMint OpenDB error: %s", err.Error())
+	}
+	defer db_utils.CloseDB(db)
+	fmt.Println("DB opened successfully.")
+
+	meta, err := memory_read.LiquidityProviderMetadata(db, "noth", "ing.")
+	if err != nil {
+		t.Errorf("TestLPMeta error: %s", err.Error())
+	}
+	if meta != nil {
+		t.Errorf("LP meta is not nil, expected nil")
+	}
+}
