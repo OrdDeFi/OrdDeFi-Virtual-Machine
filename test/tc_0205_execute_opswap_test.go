c6256cef139905e9f994d0f8add643d1eb41cfd1
satoshi <satoshi@bitcoin.org>
2023-12-29 18:14:19 +0800

    feature: db: memory_write: impl of WriteSwapInfo

--- test/tc_0205_execute_opswap_test.go
+++ test/tc_0205_execute_opswap_test.go
@@ -1,9 +1,94 @@
 package test
 
 import (
+	"OrdDeFi-Virtual-Machine/bitcoin_cli_channel"
 	"OrdDeFi-Virtual-Machine/db_utils"
+	"OrdDeFi-Virtual-Machine/virtual_machine"
+	"OrdDeFi-Virtual-Machine/virtual_machine/instruction_set"
+	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_const"
+	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_read"
+	"OrdDeFi-Virtual-Machine/virtual_machine/operations"
+	"encoding/json"
+	"fmt"
 	"testing"
 )
+
+func swapInstruction(
+	txId string,
+	lTick string, rTick string,
+	spend string, amt string) (*instruction_set.OpSwapInstruction, error) {
+	var paramsMap map[string]string
+	paramsMap = make(map[string]string)
+	paramsMap["p"] = "orddefi"
+	paramsMap["op"] = "swap"
+	paramsMap["ltick"] = lTick
+	paramsMap["rtick"] = rTick
+	paramsMap["spend"] = spend
+	paramsMap["amt"] = amt
+	jsonData, err := json.Marshal(paramsMap)
+	if err != nil {
+		return nil, err
+	}
+	commands := string(jsonData)
+	rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
+	if rawTx == nil {
+		return nil, fmt.Errorf("swapInstruction GetRawTransaction error")
+	}
+	tx := bitcoin_cli_channel.DecodeRawTransaction(*rawTx)
+	if tx == nil {
+		return nil, fmt.Errorf("swapInstruction DecodeRawTransaction error")
+	}
+	instructions, err := virtual_machine.CompileInstructions("text/plain", []byte(commands), tx, txId)
+	if err != nil {
+		return nil, fmt.Errorf("swapInstruction CompileInstructions error: %s", err.Error())
+	}
+	if len(instructions) != 1 {
+		return nil, fmt.Errorf("swapInstruction CompileInstructions error: instructions length should be 1")
+	}
+	for _, instruction := range instructions {
+		switch value := instruction.(type) {
+		case instruction_set.OpSwapInstruction:
+			return &value, nil
+		default:
+			return nil, fmt.Errorf("swapInstruction error: instruction type error, expected OpDeployInstruction")
+		}
+	}
+	return nil, fmt.Errorf("swapInstruction error: instruction type error: no instruction compiled")
+}
+
+func checkLPMeta(t *testing.T, db *db_utils.OrdDB, lTick string, rTick string) {
+	lpMeta, err := memory_read.LiquidityProviderMetadata(db, lTick, rTick)
+	if err != nil {
+		t.Errorf("checkLPMeta error: %s", err.Error())
+		return
+	}
+	println("LPMeta:", lpMeta.LTick, lpMeta.LAmt.String(), lpMeta.RTick, lpMeta.RAmt.String())
+}
+
+func checkStatusForSwap(t *testing.T, db *db_utils.OrdDB, address string, lTick string, rTick string, spendingTick string) {
+	odfiLPName := memory_const.LPNameByTicks("odfi", spendingTick)
+	lpName := memory_const.LPNameByTicks(lTick, rTick)
+	if odfiLPName == nil || lpName == nil {
+		t.Errorf("checkStatusForSwap tick error: calculate odfiLPName failed or calculate lpName failed")
+		return
+	}
+	checkLPMeta(t, db, lTick, rTick)
+	if *odfiLPName != *lpName {
+		checkLPMeta(t, db, "odfi", spendingTick)
+	}
+	lTickAvailableAmt, err := memory_read.AvailableBalance(db, lTick, address)
+	if err != nil {
+		t.Errorf("checkStatusForSwap tick error: read AvailableBalance failed %s", err.Error())
+		return
+	}
+	rTickAvailableAmt, err := memory_read.AvailableBalance(db, rTick, address)
+	if err != nil {
+		t.Errorf("checkStatusForSwap tick error: read AvailableBalance failed %s", err.Error())
+		return
+	}
+	println("User available", lTick, lTickAvailableAmt.String())
+	println("User available", rTick, rTickAvailableAmt.String())
+}
 
 func TestSwap(t *testing.T) {
 	// open db
@@ -12,4 +97,27 @@ func TestSwap(t *testing.T) {
 		t.Errorf("TestExecuteMint OpenDB error: %s", err.Error())
 	}
 	defer db_utils.CloseDB(db)
+
+	txId := "61de96170018ce878b1adf287b8ac9cf0e4f0ad8c5a69af203cc25bbde72a13e"
+	address := "bc1q2f0tczgrukdxjrhhadpft2fehzpcrwrz549u90"
+	lTick := "odfi"
+	rTick := "odgv"
+	spendingTick := "odgv"
+	amt := "10"
+	swap, err := swapInstruction(txId, lTick, rTick, spendingTick, amt)
+	if err != nil {
+		t.Errorf("generate swap instruction failed, error: %s", err.Error())
+	}
+
+	println("swap", lTick, rTick, "by", spendingTick, "for", amt)
+	println("status before swap:")
+	checkStatusForSwap(t, db, address, lTick, rTick, spendingTick)
+
+	err = operations.ExecuteOpSwap(*swap, db)
+	if err != nil {
+		t.Errorf("generate swap instruction failed, error: %s", err.Error())
+	}
+
+	println("status after swap:")
+	checkStatusForSwap(t, db, address, lTick, rTick, spendingTick)
 }
--- virtual_machine/memory/memory_write/swap.go
+++ virtual_machine/memory/memory_write/swap.go
@@ -6,6 +6,7 @@ import (
 	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_const"
 	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_read"
 	"errors"
+	"fmt"
 )
 
 func ODFISpendingTickLPName(spendingTick string) *string {
@@ -25,27 +26,46 @@ func WriteSwapInfo(
 	address string,
 ) error {
 	/*
-		4.
 		1. user spendingTick : - consumingAmt         (double-write)
 		2. user buyingTick   : + deltaY               (double-write)
 		3. odfi-spendingTick : + odfiTakerFee         (if LPMeta exist for odfi-spendingTick)
 		4. lp   spendingTick : + deltaX + lpTakerFee  (LPMeta)
 		5. lp   buyingTick   : - deltaY               (LPMeta)
+		-------------------------------------------------------------------------------------
+		3 & 4 won't be the same LP (protected by the callee `performSwap 2.1`):
+			* if odfi-spending LP is equal to current lp, add odfiTakerFee to lpTakerFee, and set odfiTakerFee to zero.
 	*/
 	var batchKV map[string]string
 	batchKV = make(map[string]string)
 	// 1. user spendingTick : - consumingAmt
-	spendingAvailable, err := memory_read.AvailableBalance(db, spendingTick, address)
-	if err != nil {
-		return err
+	{ // stack param isolation
+		spendingAvailable, err := memory_read.AvailableBalance(db, spendingTick, address)
+		if err != nil {
+			return err
+		}
+		updatedSpendingAvailable := spendingAvailable.Subtract(consumingAmt)
+		if updatedSpendingAvailable == nil {
+			return errors.New("WriteSwapInfo failed: calculate updatedSpendingAvailable failed")
+		}
+		spendingDoubleWriteKV := CoinBalanceDoubleWriteKV(spendingTick, address, updatedSpendingAvailable.String(), db_utils.AvailableSubAccount)
+		for k, v := range spendingDoubleWriteKV {
+			batchKV[k] = v
+		}
 	}
-	updatedSpendingAvailable := spendingAvailable.Subtract(consumingAmt)
-	if updatedSpendingAvailable == nil {
-		return errors.New("WriteSwapInfo failed: calculate updatedSpendingAvailable failed")
-	}
-	spendingDoubleWriteKV := CoinBalanceDoubleWriteKV(spendingTick, address, updatedSpendingAvailable.String(), db_utils.AvailableSubAccount)
-	for k, v := range spendingDoubleWriteKV {
-		batchKV[k] = v
+	// 2. user buyingTick   : + deltaY
+	{ // stack param isolation
+		buyingAvailable, err := memory_read.AvailableBalance(db, buyingTick, address)
+		if err != nil {
+			return err
+		}
+		updatedBuyingAvailable := buyingAvailable.Add(deltaY)
+		if updatedBuyingAvailable == nil {
+			return errors.New("WriteSwapInfo failed: calculate updatedBuyingAvailable failed")
+		}
+		buyingDoubleWriteKV := CoinBalanceDoubleWriteKV(buyingTick, address, updatedBuyingAvailable.String(), db_utils.AvailableSubAccount)
+		for k, v := range buyingDoubleWriteKV {
+			batchKV[k] = v
+		}
 	}
 	// 3. odfi-spendingTick : + odfiTakerFee
 	if odfiTakerFee.IsZero() == false {
@@ -73,13 +93,64 @@ func WriteSwapInfo(
 		if odfiLPJson == nil {
 			return errors.New("WriteSwapInfo failed: generate odfiLPJson failed")
 		}
-		lpName := memory_const.LPNameByTicks("odfi", spendingTick)
-		if lpName == nil {
-			return errors.New("WriteSwapInfo failed: calculate odfi-spendingTick lpName failed")
+		odfiLPName := memory_const.LPNameByTicks("odfi", spendingTick)
+		if odfiLPName == nil {
+			return errors.New("WriteSwapInfo failed: calculate odfiLPName failed")
 		}
-		odfiLPMetaKey := memory_const.LPMetaDBPath(*lpName)
+		odfiLPMetaKey := memory_const.LPMetaDBPath(*odfiLPName)
 		batchKV[odfiLPMetaKey] = *odfiLPJson
 	}
-	err = db.StoreKeyValues(batchKV)
+	// 4. lp   spendingTick : + deltaX + lpTakerFee  (LPMeta)
+	// 5. lp   buyingTick   : - deltaY               (LPMeta)
+	{ // stack param isolation
+		lpMeta, err := memory_read.LiquidityProviderMetadata(db, buyingTick, spendingTick)
+		if err != nil {
+			return err
+		}
+		if spendingTick == lpMeta.LTick {
+			lpMeta.LAmt = lpMeta.LAmt.Add(deltaX)
+			if lpMeta.LAmt == nil {
+				return fmt.Errorf("WriteSwapInfo failed: calculate lpMeta.LAmt error (+deltaX %s)", deltaX.String())
+			}
+			lpMeta.LAmt = lpMeta.LAmt.Add(lpTakerFee)
+			if lpMeta.LAmt == nil {
+				return fmt.Errorf("WriteSwapInfo failed: calculate lpMeta.LAmt error (+lpTakerFee %s)", lpTakerFee.String())
+			}
+			lpMeta.RAmt = lpMeta.RAmt.Subtract(deltaY)
+			if lpMeta.RAmt == nil {
+				return fmt.Errorf("WriteSwapInfo failed: calculate lpMeta.RAmt error (-deltaY %s)", deltaY.String())
+			}
+		} else if spendingTick == lpMeta.RTick {
+			lpMeta.RAmt = lpMeta.RAmt.Add(deltaX)
+			if lpMeta.RAmt == nil {
+				return fmt.Errorf("WriteSwapInfo failed: calculate lpMeta.RAmt error (+deltaX %s)", deltaX.String())
+			}
+			lpMeta.RAmt = lpMeta.RAmt.Add(lpTakerFee)
+			if lpMeta.RAmt == nil {
+				return fmt.Errorf("WriteSwapInfo failed: calculate lpMeta.RAmt error (+lpTakerFee %s)", lpTakerFee.String())
+			}
+			lpMeta.LAmt = lpMeta.LAmt.Subtract(deltaY)
+			if lpMeta.LAmt == nil {
+				return fmt.Errorf("WriteSwapInfo failed: calculate lpMeta.LAmt error (-deltaY %s)", deltaY.String())
+			}
+
+		} else {
+			return errors.New("WriteSwapInfo failed: spending tick error")
+		}
+		lpJson, err := lpMeta.JsonString()
+		if err != nil {
+			return err
+		}
+		if lpJson == nil {
+			return errors.New("WriteSwapInfo failed: generate lpJson failed")
+		}
+		lpName := memory_const.LPNameByTicks(buyingTick, spendingTick)
+		if lpName == nil {
+			return errors.New("WriteSwapInfo failed: calculate lpName failed")
+		}
+		odfiLPMetaKey := memory_const.LPMetaDBPath(*lpName)
+		batchKV[odfiLPMetaKey] = *lpJson
+	}
+	err := db.StoreKeyValues(batchKV)
 	return err
 }
