16e4f1ca3f2feccd14cc4ea6c922b95c84add4e2
satoshi <satoshi@bitcoin.org>
2024-01-01 13:52:55 +0800

    feature: updater: add copy dir function in file_utils

--- file_utils/copy_file.go
+++ file_utils/copy_file.go
@@ -3,6 +3,7 @@ package file_utils
 import (
 	"io"
 	"os"
+	"path/filepath"
 )
 
 func CopyFile(src, dest string) error {
@@ -30,6 +31,38 @@ func CopyFile(src, dest string) error {
 		_, err = destinationFile.Write(buffer[:bytesRead])
 		if err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+func CopyDir(srcPath, destPath string) error {
+	err := os.RemoveAll(destPath)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(destPath, 0755)
+	if err != nil {
+		return err
+	}
+	entries, err := os.ReadDir(srcPath)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		srcFile := filepath.Join(srcPath, entry.Name())
+		destFile := filepath.Join(destPath, entry.Name())
+		if entry.IsDir() {
+			// recursive copy dir
+			err := CopyDir(srcFile, destFile)
+			if err != nil {
+				return err
+			}
+		} else {
+			err := CopyFile(srcFile, destFile)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
--- test/tc_0006_copy_file_test.go
+++ test/tc_0006_copy_file_test.go
@@ -7,10 +7,17 @@ import (
 )
 
 func TestCopyFile(t *testing.T) {
-	err := file_utils.CopyFile("/Users/satoshi/large.file", "/Users/satoshi/largecopy.file")
+	err := file_utils.CopyFile("/Users/satoshi/large.file", "/Users/satoshi/large_copy.file")
 	if err != nil {
 		if strings.HasSuffix(err.Error(), "no such file or directory") == false {
 			t.Errorf("Copy file error: %s", err.Error())
 		}
+	}
+}
+
+func TestCopyDir(t *testing.T) {
+	err := file_utils.CopyDir("/Users/satoshi/dir", "/Users/satoshi/dir_copy")
+	if err != nil {
+		t.Errorf("Copy dir error: %s", err.Error())
 	}
 }
