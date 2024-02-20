6d2ac644dec6d518ac12d56c8c3dc4ecebc75067
satoshi <satoshi@bitcoin.org>
2024-02-20 16:25:58 +0800

    feature: vm: concurrent calling bitcoin-cli at indexing

--- updater/updater.go
+++ updater/updater.go
@@ -2,12 +2,41 @@ package updater
 
 import (
 	"OrdDeFi-Virtual-Machine/bitcoin_cli_channel"
+	"OrdDeFi-Virtual-Machine/concurrent"
 	"OrdDeFi-Virtual-Machine/db_utils"
 	"OrdDeFi-Virtual-Machine/inscription_parser"
 	"OrdDeFi-Virtual-Machine/virtual_machine"
 	"OrdDeFi-Virtual-Machine/virtual_machine/operations"
 	"errors"
+	"sync"
 )
+
+func prepareRawTransaction(blockTx []string) map[string]string {
+	tasks := make(chan concurrent.Task, 50)
+	var wg sync.WaitGroup
+	storage := concurrent.NewResultStorage()
+
+	concurrent.StartWorkerPool(tasks, 10, &wg, storage)
+	for _, txId := range blockTx {
+		txId := txId
+		tasks <- func() (string, string) {
+			rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
+			if rawTx == nil {
+				return txId, ""
+			}
+			return txId, *rawTx
+		}
+	}
+	close(tasks)
+	wg.Wait()
+
+	result := storage.Results
+	if len(blockTx) != len(result) {
+		println("prepareRawTransaction error: len(blockTx) != len(result), expected", len(blockTx), "got", len(result))
+		return nil
+	}
+	return result
+}
 
 func UpdateBlockNumber(blockNumber int, blockHash *string, dataDir string, logDir string, verbose bool) error {
 	var err error
@@ -39,14 +68,21 @@ func UpdateBlockNumber(blockNumber int, blockHash *string, dataDir string, logDi
 		return err
 	}
 	defer db_utils.CloseDB(logDB)
+
+	rawTransactions := prepareRawTransaction(block.Tx)
 	// enum txId, execute operations if exist
 	for txIndex, txId := range block.Tx {
-		rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
-		if rawTx == nil {
+		//rawTx := bitcoin_cli_channel.GetRawTransaction(txId)
+		//if rawTx == nil {
+		//	err = errors.New("GetRawTransaction Failed")
+		//	break
+		//}
+		rawTxStr := rawTransactions[txId]
+		if rawTxStr == "" {
 			err = errors.New("GetRawTransaction Failed")
 			break
 		}
-		tx := bitcoin_cli_channel.DecodeRawTransaction(*rawTx)
+		tx := bitcoin_cli_channel.DecodeRawTransaction(rawTxStr)
 		if tx == nil {
 			err = errors.New("ParseRawTransaction -> DecodeRawTransaction Failed")
 			break
