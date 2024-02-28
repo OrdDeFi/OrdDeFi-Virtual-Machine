b8fb3e721584b0308d12ab9ac026754f44727fae
satoshi <satoshi@bitcoin.org>
2024-02-28 23:10:03 +0800

    feature: subcommand: GetAddressUTXOTransferList impl

--- subcommands/checkutxotransfer.go
+++ subcommands/checkutxotransfer.go
@@ -34,6 +34,21 @@ func CheckUTXOTransfer(utxo string, dataDir string) {
 }
 
 func GetAddressUTXOTransferList(address string, dataDir string) {
+	db, err := db_utils.OpenDB(dataDir)
+	if err != nil {
+		println("open db error:", err.Error())
+		os.Exit(32)
+	}
+	defer db_utils.CloseDB(db)
+
+	r, err := memory_read.AllUTXOTransferForAddress(db, address)
+	if err != nil {
+		println("GetUTXOTransferList read AllAddressBalanceForCoin error:", err.Error())
+		os.Exit(33)
+	}
+	for k, v := range r {
+		println(k, ":", v)
+	}
 }
 
 type utxoListSortingPair struct {
