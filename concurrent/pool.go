6dde6454f84ac4fee39830034bd8cbe268369fad
satoshi <satoshi@bitcoin.org>
2024-03-01 05:25:59 +0800

    optimize: making pool.ResultStorage public

--- concurrent/pool.go
+++ concurrent/pool.go
@@ -5,22 +5,22 @@ import "sync"
 // Task is now defined as a function that returns a key-value pair (string, string).
 type Task func() (string, string)
 
-// resultStorage is a struct that encapsulates a map and a mutex.
+// ResultStorage is a struct that encapsulates a map and a mutex.
 // The map stores task results, and the mutex ensures safe concurrent access.
-type resultStorage struct {
+type ResultStorage struct {
 	sync.Mutex
 	Results map[string]string
 }
 
 // NewResultStorage creates a new resultStorage instance.
-func NewResultStorage() *resultStorage {
-	return &resultStorage{
+func NewResultStorage() *ResultStorage {
+	return &ResultStorage{
 		Results: make(map[string]string),
 	}
 }
 
 // store safely stores a key-value pair in the results map.
-func (rs *resultStorage) store(key, value string) {
+func (rs *ResultStorage) store(key, value string) {
 	rs.Lock()
 	defer rs.Unlock()
 	rs.Results[key] = value
@@ -28,7 +28,7 @@ func (rs *resultStorage) store(key, value string) {
 
 // StartWorkerPool starts a pool with a fixed number of worker goroutines and waits for all tasks to complete.
 // It takes a resultStorage pointer to store task results.
-func StartWorkerPool(tasks chan Task, numberOfWorkers int, wg *sync.WaitGroup, storage *resultStorage) {
+func StartWorkerPool(tasks chan Task, numberOfWorkers int, wg *sync.WaitGroup, storage *ResultStorage) {
 	for i := 0; i < numberOfWorkers; i++ {
 		wg.Add(1)
 		go worker(tasks, wg, storage)
@@ -36,7 +36,7 @@ func StartWorkerPool(tasks chan Task, numberOfWorkers int, wg *sync.WaitGroup, s
 }
 
 // worker is a worker goroutine in the pool, responsible for executing tasks and storing their results.
-func worker(tasks chan Task, wg *sync.WaitGroup, storage *resultStorage) {
+func worker(tasks chan Task, wg *sync.WaitGroup, storage *ResultStorage) {
 	defer wg.Done()
 	for task := range tasks {
 		key, value := task()      // Execute the task and get the result.
