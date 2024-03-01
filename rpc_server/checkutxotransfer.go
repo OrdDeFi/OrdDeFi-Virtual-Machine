f773a588ed4cd5dc0a310c203bf079309fb8ecac
satoshi <satoshi@bitcoin.org>
2024-03-02 00:10:20 +0800

    feature: RPC server: checkUTXOTransfer RPC impl

--- rpc_server/checkutxotransfer.go
+++ rpc_server/checkutxotransfer.go
@@ -1,6 +1,7 @@
 package rpc_server
 
 import (
+	"OrdDeFi-Virtual-Machine/subcommands"
 	"encoding/json"
 	"net/http"
 )
@@ -18,7 +19,21 @@ checkUTXOTransfer
 */
 func checkUTXOTransfer(w http.ResponseWriter, r *http.Request) {
 	utxoStr := r.URL.Query().Get("utxo")
-
 	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(map[string]string{"utxo": utxoStr})
+	addressPtr, tickPtr, amount, err := subcommands.CheckUTXOTransferData(utxoStr, glDataDir)
+	if err != nil {
+		_ = json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return
+	}
+	if addressPtr == nil || tickPtr == nil || amount == nil {
+		_ = json.NewEncoder(w).Encode(map[string]string{"utxo": utxoStr})
+		return
+	}
+	var jsonRes map[string]interface{}
+	jsonRes = make(map[string]interface{})
+	jsonRes["utxo"] = utxoStr
+	jsonRes["address"] = *addressPtr
+	jsonRes["tick"] = *tickPtr
+	jsonRes["amount"] = amount.String()
+	_ = json.NewEncoder(w).Encode(jsonRes)
 }
