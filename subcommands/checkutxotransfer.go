6517f356990f69cbc0cb03b1cab4282620fddfda
satoshi <satoshi@bitcoin.org>
2024-03-01 23:37:22 +0800

    feature: RPC server: CheckUTXOTransferData impl

--- subcommands/checkutxotransfer.go
+++ subcommands/checkutxotransfer.go
@@ -2,16 +2,18 @@ package subcommands
 
 import (
 	"OrdDeFi-Virtual-Machine/db_utils"
+	"OrdDeFi-Virtual-Machine/safe_number"
 	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_read"
+	"fmt"
 	"os"
 	"strings"
 )
 
-func CheckUTXOTransfer(utxo string, dataDir string) {
+func CheckUTXOTransferData(utxo string, dataDir string) (*string, *string, *safe_number.SafeNum, error) {
 	db, err := db_utils.OpenDB(dataDir)
 	if err != nil {
 		println("open db error:", err.Error())
-		os.Exit(6)
+		return nil, nil, nil, err
 	}
 	defer db_utils.CloseDB(db)
 
@@ -19,13 +21,26 @@ func CheckUTXOTransfer(utxo string, dataDir string) {
 	address, tick, amount, err := memory_read.UTXOCarryingBalance(db, components[0])
 	if err != nil {
 		println("CheckUTXOTransfer read UTXOCarryingBalance error:", err.Error())
-		os.Exit(7)
+		return nil, nil, nil, err
 	}
 	if address == nil || tick == nil || amount == nil {
-		println("No assets in UTXO:", utxo)
+		return nil, nil, nil, nil
+	}
+	return address, tick, amount, nil
+}
+
+func CheckUTXOTransfer(utxo string, dataDir string) {
+	addressPtr, tickPtr, amount, err := CheckUTXOTransferData(utxo, dataDir)
+	if err != nil {
+		println("CheckUTXOTransferData error:", err.Error())
+		os.Exit(6)
+	}
+
+	if addressPtr == nil || tickPtr == nil || amount == nil {
+		fmt.Println("No assets in UTXO:", utxo)
 		return
 	}
-	println("From address:", *address)
-	println("Tick:", *tick)
-	println("Amount:", amount.String())
+	fmt.Println("From address:", *addressPtr)
+	fmt.Println("Tick:", *tickPtr)
+	fmt.Println("Amount:", amount.String())
 }
