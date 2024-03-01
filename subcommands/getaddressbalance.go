7ed69dd5b9fd704a93a73052c121ce57b6c61e1f
satoshi <satoshi@bitcoin.org>
2024-03-01 22:31:26 +0800

    feature: RPC server: GetAddressBalanceData impl

--- subcommands/getaddressbalance.go
+++ subcommands/getaddressbalance.go
@@ -3,23 +3,32 @@ package subcommands
 import (
 	"OrdDeFi-Virtual-Machine/db_utils"
 	"OrdDeFi-Virtual-Machine/virtual_machine/memory/memory_read"
+	"fmt"
 	"os"
 )
 
-func GetAddressBalance(address string, dataDir string) {
+func GetAddressBalanceData(address string, dataDir string) (map[string]string, error) {
 	db, err := db_utils.OpenDB(dataDir)
 	if err != nil {
 		println("open db error:", err.Error())
-		os.Exit(8)
+		return nil, err
 	}
 	defer db_utils.CloseDB(db)
 
 	r, err := memory_read.AllCoinBalanceForAddress(db, address)
 	if err != nil {
 		println("GetAddressBalanceParam read AllCoinBalanceForAddress error:", err.Error())
-		os.Exit(9)
+		return nil, err
+	}
+	return r, err
+}
+
+func GetAddressBalance(address string, dataDir string) {
+	r, err := GetAddressBalanceData(address, dataDir)
+	if err != nil {
+		os.Exit(8)
 	}
 	for k, v := range r {
-		println(k, ":", v)
+		fmt.Println(k, ":", v)
 	}
 }
